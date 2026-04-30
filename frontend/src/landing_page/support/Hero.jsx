b3adb111f104ea3b74c9e91472e46ede48a44be3
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-04-30 18:11:52 +0530

    Fixed tickets screen color

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -9,18 +9,22 @@ const Hero = () => {
                     <h5 className="fs-3">
                         Search for an answer or browse help topics to create a ticket
                     </h5>
-                    <input placeholder="Eg. how do I activate F&O" />
+                    <input placeholder="Eg. how do I activate F&O" className='bg-white rounded text-gray-900 px-4 py-3 w-1/2 my-2' disabled/>
                     <br />
-                    <a href="#" className='my-1'>Track account opening</a>
-                    <a href="#" className='my-1'>Track segment activation</a>
-                    <a href="#" className='my-1'>Intraday margins</a>
-                    <a href="#" className='my-1'>Kite user manual</a>
+                    <div className='flex justify-items-start items-start'>
+                    <a href="#" className='my-1 underline'>Track account opening</a>
+                    <a href="#" className='my-1 underline'>Track segment activation</a>
+                    <a href="#" className='my-1 underline'>Intraday margins</a>
+                    <a href="#" className='my-1 underline'>Kite user manual</a>
+                    </div>
                 </div>
                 <div className="w-1/2 p-3 flex flex-col items-start ">
                     <h4 className="text-2xl font-bold">Track Tickets</h4>
                     <h5 className="my-1">Featured</h5>
-                    <a href="#" className='my-1'>Current Takeovers and Delisting - January 2026</a>
-                    <a href="#" className='my-1'>Latest Intraday leverages - MIS & CO</a>
+                    <ol className='list-decimal pl-10'>
+                    <li><a href="#" className='my-1 underline'>Current Takeovers and Delisting - January 2026</a></li>
+                    <li><a href="#" className='my-1 underline'>Latest Intraday leverages - MIS & CO</a></li>
+                    </ol>
                 </div>
             </div>
         </section>
